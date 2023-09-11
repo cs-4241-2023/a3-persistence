ae850042797d09cb11e5a9a84ec03ffc2b9d087e
Tate <tate.donne@gmail.com>
2023-09-11 12:17:57 -0400

    Intial commit

---
+++ public/js/script.js
@@ -0,0 +1,158 @@
+// FRONT-END (CLIENT) JAVASCRIPT HERE
+const submit = async function( event) {
+    // stop form submission from trying to load
+    // a new .html page for displaying results...
+    // this was the original browser behavior and still
+    // remains to this day
+    event.preventDefault()
+
+
+    const form = document.querySelector('form')
+
+    let task=form["Task"].value;
+    let deadline=new Date(form["Deadline"].value).toLocaleDateString("en-US");
+    let creationDate=new Date(form["CreationDate"].value).toLocaleDateString("en-US");
+
+    let taskValid=task!=="" && task!==undefined;
+    let dateValid=creationDate.value !== "";
+    let deadlineValid=deadline.value !== "";
+
+    if(!taskValid){
+        alert("Task is invalid");
+    }
+    if(!dateValid){
+        alert("Date is invalid");
+    }
+    if(!deadlineValid){
+        alert("Deadline is invalid");
+    }
+
+    if(taskValid && dateValid && deadlineValid){
+        let json = { task, creationDate, deadline};
+        let body = JSON.stringify( json );
+        console.log(body);
+        const response = await fetch( '/json', {
+            method:'POST',
+            url:'/addtask',
+            body
+        })
+        const data = await response.json();
+        LoadFromServer(data);
+        ClearForm();
+    }
+}
+
+function JudgePriority(deadline){
+    let today=new Date();
+    let dateDiff=DateDifference(today,new Date(deadline));
+    let priority="High Priority"
+    if(dateDiff>2){
+        priority="Low Priority";
+    }else if(dateDiff>1){
+        priority="Medium Priority";
+    }else if(isNaN(dateDiff)){
+        priority="NaN";
+    }
+    return priority;
+}
+
+function DateDifference(day1,day2){
+    let date1=Date.UTC(day1.getFullYear(), day1.getMonth(), day1.getDate());
+    let date2= Date.UTC(day2.getFullYear(), day2.getMonth(), day2.getDate());
+
+    return Math.floor((date2 - date1) / (1000 * 60 * 60 * 24));
+}
+
+function CreateFirstRow(){
+    let row=document.createElement("tr");
+    row.append(CreateHeaderCell(""));
+    row.append(CreateHeaderCell("Task"));
+    row.append(CreateHeaderCell("Creation Date"));
+    row.append(CreateHeaderCell("Deadline"));
+    row.append(CreateHeaderCell("Priority"));
+    return row;
+}
+
+function CreateHeaderCell(cellInfo){
+    const cell = document.createElement('th');
+    cell.innerHTML = `<p>${cellInfo}</p>`;
+    return cell;
+}
+
+function CreateRow(task,creationDate,deadline,priority,jsonString){
+    let row=document.createElement("tr");
+    row.append(CreateDeleteButton(jsonString));
+    row.append(CreateCell(task));
+    row.append(CreateCell(creationDate));
+    row.append(CreateCell(deadline));
+    row.append(CreateCell(priority));
+    return row;
+}
+
+function deleteRow(jsonString){
+    console.log(jsonString)
+    fetch( "/deletetask", {
+        method:"POST",
+        body:jsonString
+    }).then(()=>{
+        console.log('task deleted')
+    })
+}
+
+function ClearForm(){
+    const form = document.querySelector( '#addItemContainer' );
+    form.Task.value="";
+    form.Deadline.value="";
+    form.CreationDate.value="";
+}
+function CreateDeleteButton(jsonString){
+    const cell = document.createElement('td');
+    cell.className="delete";
+
+    const button=document.createElement('button');
+    button.className="delete-button";
+    button.innerHTML = <i class="fa-solid fa-trash"></i>;
+    button.onclick= () => {
+        deleteRow(JSON.stringify(jsonString));
+    }
+
+    cell.append(button);
+    return cell;
+}
+
+function CreateCell(cellInfo){
+    const cell = document.createElement('td');
+    cell.innerHTML = `<p>${cellInfo}</p>`;
+    return cell;
+}
+
+function LoadFromServer(data){
+    const table=document.createElement("table");
+    let firstRow=CreateFirstRow();
+
+    table.append(firstRow);
+    data.forEach(item => {
+        console.log(item);
+        let row=CreateRow(item["task"],
+            item["creationDate"],
+            item["deadline"],
+            JudgePriority(item["deadline"]),
+            item.toString());
+        table.append(row);
+    });
+
+    let htmlTable=document.getElementById("task-table");
+    htmlTable.replaceChildren();
+    htmlTable.append(table);
+}
+
+window.onload = async function() {
+    const addButton = document.querySelector(".add-button");
+    addButton.onclick = submit;
+
+    const response = await fetch( '/json', {
+        method:'GET'
+    })
+    const data = await response.json();
+    LoadFromServer(data);
+}
---
+++ server.improved.js
@@ -0,0 +1,127 @@
+const http = require( 'http' ),
+    fs   = require( 'fs' ),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library if you're testing this on your local machine.
+    // However, Glitch will install it automatically by looking in your package.json
+    // file.
+    mime = require( 'mime' ),
+    dir  = 'public/',
+    port = 3000
+
+const appdata = [
+  { 'task': 'homework', 'creationDate': '9/01/2023', 'deadline': '9/08/2023'},
+  { 'task': 'taxes', 'creationDate': '9/01/2023', 'deadline': '9/09/2023' },
+  { 'task': 'chores', 'creationDate': '9/01/2023', 'deadline': '9/10/2023' }
+]
+
+const server = http.createServer( function( request,response ) {
+  if( request.method === 'GET' ) {
+    handleGet( request, response )
+  }else if( request.method === 'POST' ){
+    handlePost( request, response );
+  }else if( request.method === 'DELETE' ){
+    handleDelete( request, response );
+  }
+})
+
+const handleDelete = function( request, response ) {
+  console.log("Handle Delete");
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+    dataString += data
+  })
+
+  request.on( 'end', function() {
+    let data=JSON.parse( dataString );
+    console.log(data);
+  })
+
+  /*request.on( 'data', function( data ) {
+    dataString += data
+  })
+
+  console.log(dataString);
+
+  request.on( 'end', function() {
+    let data = JSON.parse(dataString);
+    let index=appdata.indexOf(data);
+    console.log(data);
+    console.log(index);
+    if (index > -1) {
+      appdata.splice(index, 1);
+    }
+  });*/
+}
+
+const handleGet = function( request, response ) {
+  SortData();
+  const filename = dir + request.url.slice( 1 )
+  if( request.url === '/' ) {
+    sendFile( response, 'public/index.html' )
+  }else if (request.url === '/json' ) {
+    handleGetData(request, response);
+  }else{
+    sendFile( response, filename )
+  }
+}
+
+function handleGetData(request, response){
+  response.setHeader('Content-Type', 'application/json');
+  response.end(JSON.stringify(appdata));
+}
+
+const handlePost = function( request, response ) {
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+    dataString += data
+  })
+
+  request.on( 'end', function() {
+    console.log(dataString)
+    if (request.url === '/addtask'){
+      appdata.push(JSON.parse(dataString))
+    } else {  //deleteItem
+      let index = appdata.indexOf(JSON.parse(dataString))
+      appdata.splice(index, 1)
+    }
+
+    SortData();
+    response.writeHead( 200, "OK", {'Content-Type': 'text/json' })
+    response.end( JSON.stringify( appdata ) )
+  })
+}
+
+function SortData(){
+    appdata.sort(function(a, b){
+      var c = new Date(a.deadline);
+      var d = new Date(b.deadline);
+      return c-d;
+    });
+}
+
+
+const sendFile = function( response, filename ) {
+  const type = mime.getType( filename )
+
+  fs.readFile( filename, function( err, content ) {
+
+    // if the error = null, then we've loaded the file successfully
+    if( err === null ) {
+
+      // status code: https://httpstatuses.com
+      response.writeHeader( 200, { 'Content-Type': type })
+      response.end( content )
+
+    }else{
+
+      // file not found, error code 404
+      response.writeHeader( 404 )
+      response.end( '404 Error: File Not Found' )
+
+    }
+  })
+}
+
+server.listen( process.env.PORT || port )
