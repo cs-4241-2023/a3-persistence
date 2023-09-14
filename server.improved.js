b830ec1bb5817a93530f70bd8d51939fcf761633
Amitai Erfanian <aberfanian@outlook.com>
2023-09-14 00:23:27 -0400

    Update server.improved.js

--- server.improved.js
+++ server.improved.js
@@ -69,7 +69,7 @@ passport.deserializeUser((obj, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:3000/auth/google/callback',
+  callbackURL: 'https://cs4241-assignment3.onrender.com/auth/google/callback',
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
