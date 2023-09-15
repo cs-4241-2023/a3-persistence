d0c451105e6ddee9cda2f90acd233e8c8a540d13
SmeeBoi <sameer.desai.boston@gmail.com>
2023-09-15 12:51:49 -0400

    more readable code with extra functions for delete and edit

--- express.server.js
+++ express.server.js
@@ -1,59 +1,79 @@
-const express = require('express'),
-    app = express()
+'use strict';
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Static files from public directory
-app.use(express.static('public'))
+app.use(express.static('public')) // Static files from public directory
 app.use(express.json()) // for parsing application/json
 
-// serverPlayerLog is the test data that is stored on server
-let serverPlayerLog = [
+// List of Players on Server
+let playerList = [
     { name: 'example-player', color: 'red', score: 0, rank: 0 }
 ]
-
-
-// Routes
 
 // GET
 app.get('/', (req, res) => res.send('Handled GET request'))
 
 // POST 
 app.post('/submit', express.json(), (req, res) => {
-    serverPlayerLog.push(req.body) // Add new player to serverPlayerLog 
-    rankPlayers(serverPlayerLog);
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(serverPlayerLog))
+    playerList.push(req.body) // Add player to  playerList 
+    respond(res, playerList);
 })
 
 // DELETE
 app.delete('/delete', express.json(), (req, res) => {
-    // Delete player from serverPlayerLog
-    serverPlayerLog = serverPlayerLog.filter(player => player.name !== req.body.name)
-    rankPlayers(serverPlayerLog);
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(serverPlayerLog))
+    deletePlayer(playerList, req.body)
+    respond(res, playerList);
 })
 
 // PUT
 app.put('/edit', express.json(), (req, res) => {
-    // Update player names in serverPlayerLog
-    serverPlayerLog.forEach(player => {
-        if (player.name === req.body.name) {
-            player.name = req.body.newName; // Update player name
-        }
-    })
-    rankPlayers(serverPlayerLog);
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(serverPlayerLog))
+    editPlayer(playerList, req.body)
+    respond(res, playerList);
 })
 
 // Start server
-app.listen(process.env.PORT || 3000)
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`);
+});
 
-// Rank Players sort serverPlayerLog and gives each player a rank
-function rankPlayers(serverPlayerLog) {
-    serverPlayerLog.sort((a, b) => (a.score < b.score) ? 1 : -1)
-    serverPlayerLog.forEach((player, index) => {
+// Rank Players sort  playerList and gives each player a rank
+function rankPlayers(playerList) {
+    playerList.sort((a, b) => (a.score < b.score) ? 1 : -1)
+    playerList.forEach((player, index) => {
         player.rank = index + 1;
     })
-    return serverPlayerLog;
+    return playerList;
+}
+
+// Send response json to client
+function sendResponse(res, data) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(data));
+}
+
+// respondToClient function
+function respond(res, playerList) {
+    rankPlayers(playerList);
+    sendResponse(res, playerList);
+}
+
+// Delete player function
+function deletePlayer(playerList, playerToDelete) {
+    playerList.forEach(player => {
+        if (playerToDelete.name === player.name) {
+            playerList.splice(playerList.indexOf(player), 1)
+        }
+    })
+    return playerList;
+}
+
+// Edit player function
+function editPlayer(playerList, playerToEdit) {
+    playerList.forEach(player => {
+        if (player.name === playerToEdit.name) {
+            player.name = playerToEdit.newName;
+        }
+    })
+    return playerList;
 }
