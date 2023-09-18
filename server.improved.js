000231e80565add7770ec16b213a90abf22e6a97
Sombero <73486390+Sombero@users.noreply.github.com>
2023-09-18 12:03:57 -0400

    Added Express / Files
    Moved files into new repo, added express and a git ignore.
    
    (Gitignore is improperly labeled)

---
+++ server.improved.js
@@ -0,0 +1,46 @@
+
+// server (express code)
+const express = require('express'),
+      app = express(),
+      appdata = [
+        {'yourname': 'Justin', 'username': 'Sombero', 'email': 'jwonoski2@wpi.edu', 'position': 'DPS'},
+        {'yourname': 'Mason', 'username': 'Sneke', 'email': 'mSneke@wpi.edu', 'position': 'Support'},
+        {'yourname': 'Tim', 'username': 'Robo', 'email': 'tRobo@wpi.edu', 'position': 'Tank'} 
+      ]
+
+      app.use( express.static( 'public' ) )
+      app.use( express.static( 'views'  ) )
+      app.use( express.json() )      
+
+app.get( '/get',express.json(), ( req, res ) => {
+  res.status( 200 ).json(appdata)
+})
+
+app.post( '/submit', express.json(), ( req, res ) => {
+  // our request object now has a 'json' field in it from our previous middleware
+  console.log( req.body )
+  appdata.push( req.body )  
+  res.writeHead( 200, { 'Content-Type': 'application/json'})
+  res.end( req.json )
+})
+
+app.delete( '/delete', express.json(), ( req, res ) => {
+    //This deserves to be pee. Don't debate with me.
+    const pee = req.body
+    const num = Number(pee.index)
+    console.log(num)
+      appdata.splice(num, 1)
+  
+  res.status( 200 ).json(appdata)
+})
+
+app.put( '/edit', express.json(),( req, res ) => {
+  const playerinfo = req.body
+  const index = playerinfo.index
+    appdata[index] = playerinfo.playerdata
+  res.status( 200 ).json(appdata)
+});
+
+
+const listener = app.listen( process.env.PORT || 3000 )
+
