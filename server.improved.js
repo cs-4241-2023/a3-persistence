331e05bf5bc137b035bc970c5a41791452d0c483
Glitch (a3-isabellapabon) <none>
2023-09-20 18:32:13 +0000

    Log-in working

--- server.improved.js
+++ server.improved.js
@@ -1,14 +1,24 @@
 // server
 const express = require("express"),
   { MongoClient, ObjectId } = require("mongodb"),
-  app = express(),
-  playlists = [
-    { season: "fall", title: "1901", artist: "Phoenix", length: "2:00" },
-  ];
+  hbs     = require( 'express-handlebars' ).engine,
+  cookie = require('cookie-session'),
+  app = express();
+
+app.use( express.urlencoded({ extended:true }) )
 
 app.use(express.static("public"));
 app.use(express.static("views"));
 app.use(express.json());
+
+app.engine( 'handlebars',  hbs() )
+app.set(    'view engine', 'handlebars' )
+app.set(    'views',       './views' )
+
+app.use( cookie({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.il1hxgz.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -70,5 +80,41 @@ app.post("/remove", async (req, res) => {
   res.status(200).json(playlistData);
   
 });
+
+app.post( '/login', (req,res)=> {
+  console.log( req.body )
+  
+  // below is *just a simple authentication example* 
+  // for A3, you should check username / password combos in your database
+  if( req.body.password === 'test' ) {
+    // define a variable that we can check in other middleware
+    // the session object is added to our requests by the cookie-session middleware
+    req.session.login = true
+
+    res.redirect( 'main.html' )
+  }else{
+    // cancel session login in case it was previously set to true
+    req.session.login = false
+    // password incorrect, send back to login page
+    res.render('index', { msg:'login failed: incorrect password', layout:false })
+  }
+})
+
+app.get( '/', (req,res) => {
+  res.render( 'index', { msg:'', layout:false })
+})
+
+// add some middleware that always sends unauthenicaetd users to the login page
+app.use( function( req,res,next) {
+  if( req.session.login === true )
+    next()
+  else
+    res.render('index', { msg:'login failed: please try again', layout:false })
+})
+
+app.get( '/main.html', ( req, res) => {
+    res.render( 'main', { msg:'success you have logged in', layout:false })
+})
+
 
 app.listen(process.env.PORT);
