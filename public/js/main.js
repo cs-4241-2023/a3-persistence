6366c0d729d8463fa5b1317cdd0132ca71a19e13
da-munchies24 <donbothermeatall16@gmail.com>
2023-09-20 13:15:20 -0400

    update

---
+++ public/js/main.js
@@ -0,0 +1,108 @@
+const submit = async function (event) {
+    event.preventDefault();
+  
+    let input = document.querySelectorAll("#Email,#Name,#Birth"),
+      json = {
+        Email: input[0].value,
+        Name: input[1].value,
+        Birth: input[2].value,
+        Age: age(input[2].value),
+      };
+  
+    function age(birth) {
+      let date = new Date(birth);
+      let diff = Date.now() - date.getTime();
+      let date1 = new Date(diff);
+      let age = Math.abs(date1.getUTCFullYear() - 1970);
+      return age;
+    }
+  
+    let response = await fetch("/submit", {
+      method: "POST",
+      body: JSON.stringify(json),
+    });
+    const text = await response.json();
+    document.querySelector("#fade").style.display = "block";
+    setTimeout(function () {
+      document.querySelector("#fade").style.display = "none";
+    }, 1500);
+  };
+  
+  const display = function (event) {
+    event.preventDefault();
+  
+    clear(event);
+  
+    let response = fetch("/display", {
+      method: "GET",
+    })
+      .then((text) => response.json())
+      .then(function (text) {
+        const list = document.getElementById("data");
+        const header = document.createElement("tr");
+        header.innerHTML = `<th>Email</th> <th>Name</th> <th>Birth</th> <th>Age</th> <th>Delete</th>`;
+        list.insertRow(header);
+        let i = 0;
+        text.forEach((d) => {
+          const item = document.createElement("tr");
+          item.innerHTML = `
+      <td>${d.Email}</td> 
+      <td>${d.Name}</td>
+      <td>${d.Birth}</td>
+      <td>${d.Age} years old</td>
+      <td>row:${i} <input type="checkbox" class="checkOnce" id="C${i}" 
+      onclick="checkedOnClick(this)" value="${d._id}"/></td>
+      `;
+          i++;
+          list.insertRow(item);
+        });
+  
+        document.body.appendChild(list);
+      });
+  };
+  
+  const clear = function (event) {
+    event.preventDefault();
+    let table = document.getElementById("data");
+    table.innerHTML = "";
+  };
+  
+  function deleterec(event) {
+    event.preventDefault();
+  
+    let data;
+  
+    const dis = document.querySelectorAll(".checkOnce");
+  
+    for (let j = 0; j < dis.length; j++) {
+      if (dis[j].checked === true) {
+        data = j;
+      }
+    }
+    let json = {
+      _id: dis[data].value,
+    };
+    let response = fetch("/delete", {
+      method: "POST",
+      body: JSON.stringify(json),
+    })
+      .then((text) => response.json())
+      .then(function () {
+        document.querySelector("#dell").style.display = "block";
+        setTimeout(function () {
+          document.querySelector("#dell").style.display = "none";
+        }, 1500);
+      });
+  }
+  
+  window.onload = function () {
+    const button1 = document.querySelector("#submit");
+    const button4 = document.querySelector("#clear");
+    const button2 = document.querySelector("#display");
+    const button3 = document.querySelector("#delete");
+    button1.onclick = submit;
+    button2.onclick = display;
+    button3.onclick = deleterec;
+    button4.onclick = clear;
+  };
+  
---
+++ server.improved.js
@@ -0,0 +1,152 @@
+const express = require("express"),
+     {mongodb, ObjectId, MongoClient} = require("mongodb"),
+    dotenv= require('dotenv').config(),
+   app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+
+const url= `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}`
+const dbconnect= new MongoClient(url)
+
+let collection = null;
+let users = null;
+
+
+async function run() {
+ dbconnect.connect()
+  collection = await dbconnect.db("a3-Persistence").collection("a3");
+  users = await dbconnect.db("a3-Persistence").collection("users");  
+}
+
+
+app.post("/login", async (req, res) => {
+  const user = req.body.username;
+  let dis = await users.find(
+    { username: user },
+    { username: 1, password: 1, _id: 0 }
+  );
+  if (dis === "") {
+    res.render("login", {
+      message: "username not found, login failed!",
+      layout: false,
+    });
+  //  req.session.login = false;
+  } else {
+    if (dis.password === req.body.password) {
+      //req.session.login = true;
+      res.redirect("index.html");
+    } else {
+      res.render("login", {
+        message: "username not found, login failed!",
+        layout: false,
+      });
+     // req.session.login = false;
+    }
+  }
+});
+
+app.post("/submit", async (req, res) => {
+  const result = await collection.insertOne(req.body);
+  res.json(result);
+});
+
+app.get("/display", async (req,res)=>{
+  const results= await collection.find({}).toArray()
+  res.json(results);
+});
+
+app.post("/delete", async (req,res)=>{
+  const results= await collection.deleteOnce({
+    _id:new ObjectId(req.body._id)
+  })
+  res.json(results)
+})
+
+run()
+app.listen(process.env.PORT || 3000);
+
+
+/*
+
+
+const http = require("http"),
+  fs = require("fs"),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library if you're testing this on your local machine.
+  // However, Glitch will install it automatically by looking in your package.json
+  // file.
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
+
+let appdata = [];
+
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
+  }
+});
+
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
+
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  }
+  if (request.url === "/display") {
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end(JSON.stringify(appdata));
+  } else {
+    sendFile(response, filename);
+  }
+};
+
+
+const handlePost = function (request, response) {
+  let dataString = "";
+
+  request.on("data", function (data) {
+    dataString += data;
+  });
+
+  request.on("end", function () {
+    if (request.url === '/submit') {
+      appdata.push(JSON.parse(dataString));
+      response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+      response.end(JSON.stringify(appdata));
+    } else if (request.url === '/delete'){
+      let dataDel = JSON.parse(dataString)
+      if(dataDel.number >= 0){
+        appdata.splice(dataDel.number,1);
+       response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+      response.end(JSON.stringify({sucess: "Deletion completed"}));
+      }
+     
+    }
+  });
+};
+
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
+
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
+
+server.listen(process.env.PORT || port);
+*/
