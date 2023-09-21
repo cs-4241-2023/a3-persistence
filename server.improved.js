750dd8baa5ab4869410de0cbdf5ddd8754f5029f
Andrew Hariyanto <51850304+andrewhariyanto@users.noreply.github.com>
2023-09-21 03:47:24 -0400

    added cookies

--- server.improved.js
+++ server.improved.js
@@ -1,13 +1,46 @@
 const express = require("express"),
   { MongoClient, ObjectId } = require("mongodb"),
+  cookie  = require( 'cookie-session' ),
   app = express(),
   dir = "public/",
   port = 3000;
 
 require('dotenv').config();
 
+app.use( express.urlencoded({ extended:true }) )
+
+app.use( cookie({
+  name: 'session',
+  keys: ['100002', '31242']
+}))
 
 app.use(express.static('public'));
+
+app.post( '/login', (req,res)=> {
+  // express.urlencoded will put your key value pairs 
+  // into an object, where the key is the name of each
+  // form field and the value is whatever the user entered
+  console.log( req.body )
+  
+  // below is *just a simple authentication example* 
+  // for A3, you should check username / password combos in your database
+  if( req.body.password === 'test' ) {
+    // define a variable that we can check in other middleware
+    // the session object is added to our requests by the cookie-session middleware
+    req.session.login = true
+    
+    // since login was successful, send the user to the main content
+    // use redirect to avoid authentication problems when refreshing
+    // the page or using the back button, for details see:
+    // https://stackoverflow.com/questions/10827242/understanding-the-post-redirect-get-pattern 
+    res.redirect( 'main.html' )
+  }else{
+    // password incorrect, redirect back to login page
+    res.sendFile( __dirname + '/public/index.html' )
+  }
+})
+
+
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`
