81e8ebb9069ba4ab200e09d424b92eceda5dbc13
vuongdbrandon <57996169+bdvuong@users.noreply.github.com>
2023-09-21 17:01:02 -0400

    Consolidate server files

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express, { json } from "express";
-import { nanoid } from "nanoid";
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import "dotenv/config";
 
 const app = express();
@@ -16,27 +15,14 @@ const client = new MongoClient(uri);
 
 let collection = null;
 
-async function run() {
-  await client.connect();
-  collection = await client.db("whatToDo").collection("dev");
-}
-run();
+await initDB();
 
+// Middleware
 app.get("/init", async (req, res) => {
-  if (collection !== null) {
-    const docs = await collection.find({}).toArray()
-    res.json( docs )
-    console.log(res)
-  }
-  res.send(JSON.stringify(taskData));
+  sendTasks(req, res);
 });
 
-app.post("/login", (req, res) => {
-  console.log(req.body);
-  res.writeHead(200, { "Content-Type": "application/json" });
-});
-
-app.post("/submit", (req, res) => {
+app.post("/submit", async (req, res) => {
   let newTask = req.body;
 
   if (newTask.due === "") {
@@ -53,44 +39,63 @@ app.post("/submit", (req, res) => {
         break;
     }
   }
-  let index = taskData.findIndex((task) => task.id === newTask.id);
+  let index = taskData.findIndex((task) => task._id === newTask._id);
   taskData[index] = newTask;
 
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(taskData));
+  const cloneNewTask = { ...newTask };
+
+  delete cloneNewTask._id;
+
+  await collection.replaceOne(
+    { _id: new ObjectId(newTask._id) },
+    { ...cloneNewTask }
+  );
+
+  sendTasks(req, res);
 });
 
-app.post("/new", (req, res) => {
+// A way to optimize this is to create the note client-side, then when the note is saved you push it to the server (works for now but I can optimize it later). This may be difficult because we will not have an associated ID with a new note until it is created within mongo, instead we might want to use our own id (nanoid package)?
+app.post("/new", async (req, res) => {
   const newTask = {
     title: "New Note",
     date: new Date().toISOString().split("T")[0],
     priority: "Low",
     description: "",
     dueDate: "",
-    id: nanoid(),
   };
 
-  taskData.push(newTask);
+  await collection.insertOne(newTask);
 
-  res.writeHead(200, "OK", { "Content-Type": "text/plain" });
-  res.end(JSON.stringify(taskData));
+  sendTasks(req, res);
 });
 
-app.delete("/delete", (req, res) => {
-  taskData = taskData.filter((task) => {
-    return task.id !== req.body.taskID;
-  });
+app.delete("/delete", async (req, res) => {
+  await collection.deleteOne({ _id: new ObjectId(req.body.taskID) });
 
-  res.writeHead(200, "OK", { "Content-Type": "application/json" });
-
-  res.end(JSON.stringify(taskData));
+  sendTasks(req, res);
 });
 
 app.listen(process.env.PORT);
+
+// Helper Functions
+async function initDB() {
+  await client.connect();
+  collection = await client.db("whatToDo").collection("dev");
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  return;
+}
 
 // Simple function to add days to date
 function addDays(date, days) {
   var result = new Date(date);
   result.setDate(result.getDate() + days);
   return result;
+}
+
+async function sendTasks(req, res) {
+  if (collection !== null) {
+    const docs = await collection.find({}).toArray();
+    res.json(docs);
+  }
 }
