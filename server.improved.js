06ff2150fb9c9bc19cceedd83f64907847eedf5c
da-munchies24 <donbothermeatall16@gmail.com>
2023-09-22 12:12:29 -0400

    update

--- server.improved.js
+++ server.improved.js
@@ -1,49 +1,62 @@
 const express = require("express"),
-     {mongodb, ObjectId, MongoClient} = require("mongodb"),
-    dotenv= require('dotenv').config(),
+     {mongodb, ObjectId,MongoClient} = require("mongodb"),
+       cookie  = require( 'cookie-session' ),
+      hbs     = require( 'express-handlebars' ).engine,
    app = express();
 
+app.use( express.urlencoded({ extended:true }) )
 app.use(express.json());
 app.use(express.static("public"));
+app.engine( 'handlebars', hbs() )
+app.set('view engine', 'handlebars' )
+app.set('views','./views' )
 
 
-const url= `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}/retryWrites=true&w=majority&appName=AtlasApp`
-
+app.use(cookie({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
+require('dotenv').config()
+const url= `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}/?retryWrites=true&w=majority&appName=AtlasApp`
 const dbconnect= new MongoClient(url)
 
-let collection = null;
-let users = null;
+let collection;
+let users;
 
 
 async function run() {
- await dbconnect.connect().then(()=>console.log("DB works"))
+  try{
+ await dbconnect.connect().then(()=>console.log("db works"))
   collection = await dbconnect.db("a3-Persistence").collection("a3");
   users = await dbconnect.db("a3-Persistence").collection("users");  
+  //console.log(users)
+} catch(err){
+  console.error("database failed:", err)
+}
 }
 
 
 app.post("/login", async (req, res) => {
   const user = req.body.username;
-  let dis = await users.find(
-    { username: user },
-    { username: 1, password: 1, _id: 0 }
-  );
-  if (dis === "") {
+  const pass = req.body.password;
+  let dis = await users.findOne({$and: [{ username: user }, { password: pass }]});
+  console.log(dis)
+  if (dis == null) {
     res.render("login", {
       message: "username not found, login failed!",
       layout: false,
     });
-  //  req.session.login = false;
+    req.session.login = false;
   } else {
-    if (dis.password === req.body.password) {
-      //req.session.login = true;
+    if (dis != null) {
+      req.session.login = true;
       res.redirect("index.html");
     } else {
       res.render("login", {
         message: "username not found, login failed!",
         layout: false,
       });
-     // req.session.login = false;
+       req.session.login = false; 
     }
   }
 });
@@ -65,89 +78,22 @@ app.post("/delete", async (req,res)=>{
   res.json(results)
 })
 
+app.get("/", function (req, res) {
+    res.render("login", { msg: "", layout: false });
+  });
+  
+  app.use(function (req, res, next) {
+    if (req.session.login === true) next();
+    else
+      res.render("login", {
+        msg: "login failed, please try again",
+        layout: false,
+      });
+  });
+  
+  app.get("/index.html", (req, res) => {
+    res.render("index", { msg: "success you have logged in", layout: false });
+  });
 run()
 app.listen(process.env.PORT || 3000);
 
-
-/*
-
-
-const http = require("http"),
-  fs = require("fs"),
-  // IMPORTANT: you must run `npm install` in the directory for this assignment
-  // to install the mime library if you're testing this on your local machine.
-  // However, Glitch will install it automatically by looking in your package.json
-  // file.
-  mime = require("mime"),
-  dir = "public/",
-  port = 3000;
-
-let appdata = [];
-
-const server = http.createServer(function (request, response) {
-  if (request.method === "GET") {
-    handleGet(request, response);
-  } else if (request.method === "POST") {
-    handlePost(request, response);
-  }
-});
-
-const handleGet = function (request, response) {
-  const filename = dir + request.url.slice(1);
-
-  if (request.url === "/") {
-    sendFile(response, "public/index.html");
-  }
-  if (request.url === "/display") {
-    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
-    response.end(JSON.stringify(appdata));
-  } else {
-    sendFile(response, filename);
-  }
-};
-
-
-const handlePost = function (request, response) {
-  let dataString = "";
-
-  request.on("data", function (data) {
-    dataString += data;
-  });
-
-  request.on("end", function () {
-    if (request.url === '/submit') {
-      appdata.push(JSON.parse(dataString));
-      response.writeHead(200, "OK", { "Content-Type": "text/plain" });
-      response.end(JSON.stringify(appdata));
-    } else if (request.url === '/delete'){
-      let dataDel = JSON.parse(dataString)
-      if(dataDel.number >= 0){
-        appdata.splice(dataDel.number,1);
-       response.writeHead(200, "OK", { "Content-Type": "text/plain" });
-      response.end(JSON.stringify({sucess: "Deletion completed"}));
-      }
-     
-    }
-  });
-};
-
-const sendFile = function (response, filename) {
-  const type = mime.getType(filename);
-
-  fs.readFile(filename, function (err, content) {
-    // if the error = null, then we've loaded the file successfully
-
-    if (err === null) {
-      // status code: https://httpstatuses.com
-      response.writeHeader(200, { "Content-Type": type });
-      response.end(content);
-    } else {
-      // file not found, error code 404
-      response.writeHeader(404);
-      response.end("404 Error: File Not Found");
-    }
-  });
-};
-
-server.listen(process.env.PORT || port);
-*/
