edd615a2fd05a8e701e4ecc0f8e6f006bdec3d7a
Colinm1215 <colinm1215@gmail.com>
2023-09-23 00:23:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const GitHubStrategy = require('passport-github').Strategy;
 const app = express();
 const port = 3000;
 
-const uri = "mongodb+srv://doadmin:48OY32KB9g1Z76lT@db-mongodb-nyc1-53233-0b371da4.mongo.ondigitalocean.com/";
+const uri = "mongodb+srv://doapps-4ec1f246-ba4d-4013-aed9-b45e89750f81:l149ko35X086aHQj@db-mongodb-nyc1-53233-0b371da4.mongo.ondigitalocean.com/admin?authSource=admin&tls=true";
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
