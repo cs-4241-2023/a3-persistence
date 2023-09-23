a6d64729467dd726fd29d934150cf4510e3974b1
Nikola Grozdani <64702995+ngrozdani@users.noreply.github.com>
2023-09-23 15:35:24 -0400

    improved modify impl

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
 // FRONT-END (CLIENT) JAVASCRIPT HERE
 const submit = async function (event) {
   event.preventDefault();
@@ -27,7 +26,7 @@ const submit = async function (event) {
 }
 
 function handleValidationError() {
-  if (!validateYear()) {
+  if (!validateYear(year_input.value)) {
     alert("Please enter a valid year");
   } else {
     alert("Please fill out all fields");
@@ -77,18 +76,14 @@ function validate_form_input() {
     }
   }
   // Add additional validation checks here for the different input fields
-  if (validateYear()) {
+  if (validateYear(year_input.value)) {
     return true;
   }
   return false;
 }
 
-function validateYear() {
-  var yearInput = document.getElementById("year");
-  var yearValue = yearInput.value;
-
-  // Check if the input is a 4-digit number using a regular expression
-  var yearPattern = /^\d{4}$/;
+function validateYear(yearValue) {
+  var yearPattern = /^(?!0)\d{4}$/;
 
   if (yearPattern.test(yearValue)) {
     return true
@@ -119,47 +114,137 @@ const build_table = async function () {
           for (let i = 0; i < Object.entries(server_data).length; i++) {
             let prop_index = 0;
             let data_row = vehicle_data_table.insertRow(-1);
+            data_row.setAttribute("id", server_data[i].uuid);
 
             for (let prop_name in server_data[i]) {
               // build rows
               let table_cell = data_row.insertCell(prop_index);
+              table_cell.setAttribute("id", "td-" + prop_name);
               table_cell.innerHTML = server_data[i][prop_name];
               prop_index++;
             }
 
-            let rmBtnCell = data_row.insertCell(prop_index); // delete button
-            let removeButton = rmBtnCell.appendChild(document.createElement("button"));
-
-            let modifyBtnCell = data_row.insertCell(prop_index + 1); // modify button
+            let modifyBtnCell = data_row.insertCell(prop_index); // modify button
+            modifyBtnCell.setAttribute("id", "md-cell" + server_data[i].uuid);
             let modifyButton = modifyBtnCell.appendChild(document.createElement("button"));
 
+            let rmBtnCell = data_row.insertCell(prop_index + 1); // delete button
+            rmBtnCell.setAttribute("id", "rm-cell" + server_data[i].uuid);
+            let removeButton = rmBtnCell.appendChild(document.createElement("button"));
+
             removeButton.className = 'rm-table-btn';
-            // removeButton.id = server_data[i].uuid;
+            removeButton.id = "rm-" + server_data[i].uuid;
 
             modifyButton.className = 'md-table-btn';
-            // modifyButton.id = server_data[i].uuid;
+            modifyButton.id = "md-" + server_data[i].uuid;
 
             removeButton.innerHTML = "Remove"; // name of delete button
             removeButton.onclick = function () {
               removeEntry(server_data[i].uuid);
             };
 
-            modifyButton.innerHTML = "Modify";
+            modifyButton.innerHTML = "Modify"; // name of modify buttion
             modifyButton.onclick = function () {
-              let form_data = user_form_data();
-              if (form_data !== "") {
-                modifyEntry(server_data[i].uuid, form_data);
+
+              // get all td cells from specific row
+              let desiredRow = document.getElementById(server_data[i].uuid)
+              let tdListFromRow = desiredRow.querySelectorAll("td");
+
+              console.log(tdListFromRow);
+
+              // for each td value
+              for (var j = 0; j < tdListFromRow.length - 4; j++) {
+                let originalTDValue = tdListFromRow[j].innerHTML;
+
+                const inputTag = document.createElement("input");
+                if (tdListFromRow[j].id === "td-appointment_date") {
+                  inputTag.setAttribute("type", "date");
+                } else {
+                  inputTag.setAttribute("type", "text");
+                }
+                inputTag.setAttribute("id", tdListFromRow[j].id + server_data[i].uuid);
+                inputTag.setAttribute("class", "md-input-tags");
+                inputTag.value = originalTDValue;
+
+                // Replace the td values with the input element
+                tdListFromRow[j].innerHTML = "";
+                tdListFromRow[j].appendChild(inputTag);
+              }
+
+              // Hide the Modify and Remove button
+              document.getElementById("md-" + server_data[i].uuid).style.display = "none";
+              document.getElementById("rm-" + server_data[i].uuid).style.display = "none";
+
+              // Check if the Save button already exists
+              if (!document.getElementById("save-btn" + server_data[i].uuid)) {
+                const save_btn = document.createElement("button");
+                save_btn.setAttribute("type", "button");
+                save_btn.setAttribute("class", "save-table-btn");
+                save_btn.setAttribute("id", "save-btn" + server_data[i].uuid);
+                save_btn.setAttribute("onclick", `save_edit('${server_data[i].uuid}')`);
+                let text = document.createTextNode("Save");
+                save_btn.appendChild(text);
+
+                modifyBtnCell.appendChild(save_btn);
               } else {
-                handleValidationError();
+                const save_btn = document.getElementById("save-btn" + server_data[i].uuid);
+                save_btn.style.display = "inline-block";
+              }
+
+              // Check if cancel button already exists
+              if (!document.getElementById("cancel-btn")) {
+                const cancel_btn = document.createElement("button");
+                cancel_btn.setAttribute("type", "button");
+                cancel_btn.setAttribute("class", "cancel-table-btn");
+                cancel_btn.setAttribute("id", "cancel-btn" + server_data[i].uuid);
+                cancel_btn.setAttribute("onclick", "build_table()");
+                let text = document.createTextNode("Cancel");
+                cancel_btn.appendChild(text);
+
+                rmBtnCell.appendChild(cancel_btn);
+              } else {
+                const cancel_btn = document.getElementById("cancel-btn" + server_data[i].uuid);
+                cancel_btn.style.display = "inline-block";
               }
             }
 
-          }
+          } // for-loop
 
         }
       });
   } catch (error) {
     alert('An error occurred while fetching server data!');
+  }
+}
+
+function save_edit(uuid) {
+  console.log("Save edit function: ");
+
+  const yearInput = document.getElementById("td-year" + uuid);
+  const makeInput = document.getElementById("td-car_make" + uuid);
+  const modelInput = document.getElementById("td-model" + uuid);
+  const serviceTypeInput = document.getElementById("td-service_type" + uuid);
+  const appointmentDateInput = document.getElementById("td-appointment_date" + uuid);
+
+  let form_data = {
+    year: yearInput.value,
+    car_make: makeInput.value,
+    model: modelInput.value,
+    service_type: serviceTypeInput.value,
+    appointment_date: appointmentDateInput.value
+  }
+
+  const form_data_as_string = JSON.stringify(form_data);
+
+  if (form_data.year !== "" && form_data.car_make !== "" && form_data.model !== ""
+    && form_data.model !== "" && form_data.service_type !== "" && form_data.appointment_date !== "") {
+    if (validateYear(form_data.year)) {
+      modifyEntry(uuid, form_data_as_string);
+    } else {
+      alert("Cannot modify: Please enter a valid year!");
+    }
+  } else {
+    alert("Please fill out all fields before saving log!");
   }
 }
 
@@ -180,7 +265,7 @@ async function modifyEntry(UUID, user_data) {
       },
       body: JSON.stringify(req_data),
     });
-
+    ``
     if (response.ok) {
       console.log(`Data ${UUID} was modified`);
       form.reset();
@@ -212,6 +297,3 @@ async function removeEntry(UUID) {
     console.error('An error occurred:', error);
   }
 }
-
-// });
-
