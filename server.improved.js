d010c9c8856617ce9802ffbed363fdfeed801ee7
Mahir Sowad <mahirsowad3@gmail.com>
2023-09-23 10:12:47 -0400

    successfully connected to mongodb

--- server.improved.js
+++ server.improved.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
 let currentId = 3;
@@ -137,6 +138,31 @@ app.delete("/deleteTask", (req, res) => {
     res.end(JSON.stringify(tasksData));
   });
 });
+
+// MongoDB connection setup
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on http://localhost:3000`);
