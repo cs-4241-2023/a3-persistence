ebb1e1bf0f314baba5b0c7f88943a18891b3f546
SmeeBoi <sameer.desai.boston@gmail.com>
2023-09-23 17:03:58 -0400

    well after clicking login it takes me to edit player page. but i think approach with global username is messing it up

--- server.js
+++ server.js
@@ -4,58 +4,61 @@ const app = express();
 const { MongoClient } = require("mongodb");
 const PORT = process.env.PORT || 3000;
 const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
 const session = require('express-session') // store session data in cookies
+
+let githubUsername = undefined; // global variable for github username
 
 require('dotenv').config()
 
-// app.use(express.static('public')) // Static files from public directory
-app.use(express.static('public', { index: false })) // don't give index.html by default
+app.use(express.static('public')) // Static files from public directory
+// app.use(express.static('public', { index: false })) // don't give index.html by default
 app.use(express.json()) // For parsing application/json
 
 // MongoDB connection
-// const uri = `mongodb+srv://${process.env.TESTER}:${process.env.PASS}@${process.env.HOST}`
-// const client = new MongoClient(uri);
-// let collection = undefined;
+const uri = `mongodb+srv://${process.env.TESTER}:${process.env.PASS}@${process.env.HOST}`
+const client = new MongoClient(uri);
+let collection = undefined;
 // Set collection global value with database
-// async function run() {
-//   await client.connect()
-//   collection = client.db("testA3").collection("testList")
-// }
-// run();
+async function dbconnect() {
+  await client.connect()
+  collection = client.db("testA3").collection("testList")
+}
+dbconnect();
 
 // From video passport tutorial
 
 app.use(session({
-  secret: 'keyboard cat',
+  secret: process.env.USER_SECRET,
   resave: false,
   saveUninitialized: false, // we set to false
-  cookie: { httpOnly: true,
-  secure: false, // set secure to true when not using localhost
-  maxAge: 24 * 60 * 60 * 1000} // 24 hours
+  cookie: {
+    httpOnly: true,
+    secure: false, // set secure to true when not using localhost
+    maxAge: 24 * 60 * 60 * 1000
+  } // 24 hours
 }))
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Take user object and returns an id
-passport.serializeUser(function(user, callback) {
+passport.serializeUser(function (user, callback) {
   callback(null, user.id);
 });
 
 // Take id and returns user object
-passport.deserializeUser(function(id, callback) {
+passport.deserializeUser(function (id, callback) {
   callback(null, id);
 });
 
-
-const GitHubStrategy = require('passport-github').Strategy;
 passport.use(new GitHubStrategy({
-    clientID: 'c4ec932eca4d8d964e17', // Client ID from GitHub OAuth app
-    clientSecret: 'b0e3033db1e1d4f34e8d19cd73e5e77df7611e0d', // Client Secret from GitHub OAuth app
-    callbackURL: "http://localhost:3000/auth/github/callback" // Callback URL from GitHub OAuth app
-  },
+  clientID: process.env.GITHUB_CLIENT_ID, // Client ID from GitHub OAuth app
+  clientSecret: process.env.GITHUB_CLIENT_SECRET, // Client Secret from GitHub OAuth app
+  callbackURL: process.env.GITHUB_CALLBACK_URL // Callback URL from GitHub OAuth app
+},
   // Here is where we get the user profile (GitHub account) to be used in database
-  function(accessToken, refreshToken, profile, callback) {
+  function (accessToken, refreshToken, profile, callback) {
 
     // See if the user exists already in the database
     // User.findOrCreate({ githubId: profile.id }, function (err, user) {
@@ -66,109 +69,135 @@ passport.use(new GitHubStrategy({
   }
 ));
 
-// Middleware to check if user is authenticated
+// Middleware to check if user is authenticated with persistent session
 const isAuth = (req, res, next) => {
-  if (req.user) {
-    next()
+  console.log('inside isAuth()');
+  if (githubUsername) {
+    console.log('About to call next() and show menu');
+    next(); // Yes Auth + Yes Player Account
   } else {
-    res.redirect('/login')
+    console.log('bad auth going back to login welcom page');
+    res.redirect('/login'); // No Auth 
   }
 }
 
-// Serves dashboard.html if user is authenticated
-app.get('/',  isAuth, (req, res) => {
-  console.log(req.user);
-  res.sendFile(__dirname + '/public/dashboard.html')
+// Send to menu if user is already authenticated with cookie
+app.get('/', isAuth, (req, res) => {
+  console.log(`AMAZING about to show menu for: ${req.user.username}`);
+  res.sendFile(__dirname + '/public/menu.html');
 })
 
-// Serves login.html if user is not authenticated
+
 app.get('/login', (req, res) => {
-  if(req.user) { 
-    return res.redirect('/');
+  if (req.user) {
+    return res.redirect('/'); // Yes
+  } else {
+    res.sendFile(__dirname + '/public/index.html');
   }
-  res.sendFile(__dirname + '/public/login.html')
-})
+});
 
 // Logout route
 app.get('/logout', (req, res) => {
-  req.logOut(() => {
-    res.redirect('/login');
-  });
+  req.logOut();
+  res.redirect('/login');
 })
 
 // Middleware to check if user is authenticated
-app.get('/auth/github',passport.authenticate('github'));
+app.get('/auth/github', passport.authenticate('github'));
 
 // Callback route after GitHub authentication
 app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
+  async function (req, res) {
+    // Successful authentication
+    console.log(`Good authentication: ${req.user.username}`);
+
+    // No player account -> Serve editPlayer.html to create player account
+     // Set global variable for github username
+     githubUsername = req.user.username;
+    const existingPlayer = await collection.findOne({ githubName: req.user.username });
+    if (!existingPlayer) {
+      console.log(`No player account for: ${req.user.username}`);
+      res.redirect('/editPlayer')
+      return;
+    } else {
+      // Yes player account
+      console.log(`Redirecting to menu for: ${req.user.username}`);
+      res.redirect('/');
+    }
   });
 
+
+// redirect for /editPlayer
+app.get('/editPlayer', isAuth, (req, res) => {
+  console.log(`Edit player: ${githubUsername}`);
+  res.sendFile(__dirname + '/public/editPlayer.html')
+})
+
+
+
+// End of Passport Code --------------------------------------------------------------
 
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on Port ${PORT}`);
 });
 
+// Check database connection
+app.use((req, res, next) => {
+  if (collection !== null) {
+    next()
+  } else {
+    res.status(503).send()
+  }
+})
 
-// Database connection check middleware
-// app.use((req, res, next) => {
-//   if (collection !== null) {
-//     next()
-//   } else {
-//     res.status(503).send()
-//   }
-// })
+// POST 
+app.post('/submit', express.json(), async (req, res) => {
+  // Add githubUsername to req.body
+  req.body.githubName = githubUsername // TODO: can this be done without global variable?
+  await collection.insertOne(req.body); // Add new player to the database
+  await updatePlayersAndRespond(res);
+});
 
+// DELETE
+app.delete('/delete', express.json(), async (req, res) => {
+  const playerName = req.body.name;
+  // Use MongoDB's deleteOne method to remove the player by name
+  await collection.deleteOne({ name: playerName });
+  await updatePlayersAndRespond(res);
+});
 
+// PUT
+app.put('/edit', express.json(), async (req, res) => {
+  const playerName = req.body.name;
+  const newName = req.body.newName
+  // Update the player's name in the MongoDB collection
+  await collection.updateOne({ name: playerName }, { $set: { name: newName } });
+  updatePlayersAndRespond(res)
+});
 
-// // POST 
-// app.post('/submit', express.json(), async (req, res) => {
-//   await collection.insertOne(req.body); // Add new player to the database
-//   await updatePlayersAndRespond(res);
-// });
+// Helper function to update players and respond to client
+async function updatePlayersAndRespond(res) {
+  try {
+    // Sort players by score (highest score first)
+    const players = await collection.find({}).sort({ score: -1 }).toArray();
 
-// // DELETE
-// app.delete('/delete', express.json(), async (req, res) => {
-//   const playerName = req.body.name;
-//   // Use MongoDB's deleteOne method to remove the player by name
-//   await collection.deleteOne({ name: playerName });
-//   await updatePlayersAndRespond(res);
-// });
+    // Assign ranks to players based on their position in the sorted list
+    players.forEach((player, index) => {
+      player.rank = index + 1;
+    });
 
-// // PUT
-// app.put('/edit', express.json(), async (req, res) => {
-//   const playerName = req.body.name;
-//   const newName = req.body.newName
-//   // Update the player's name in the MongoDB collection
-//   await collection.updateOne({ name: playerName }, { $set: { name: newName } });
-//   updatePlayersAndRespond(res)
-// });
+    // Update the rank for each player in the database (optional)
+    const updatePromises = players.map(async (player) => {
+      await collection.updateOne({ _id: player._id }, { $set: { rank: player.rank } });
+    });
 
-// // Helper function to update players and respond to client
-// async function updatePlayersAndRespond(res) {
-//   try {
-//     // Sort players by score (highest score first)
-//     const players = await collection.find({}).sort({ score: -1 }).toArray();
+    await Promise.all(updatePromises);
 
-//     // Assign ranks to players based on their position in the sorted list
-//     players.forEach((player, index) => {
-//       player.rank = index + 1;
-//     });
-
-//     // Update the rank for each player in the database (optional)
-//     const updatePromises = players.map(async (player) => {
-//       await collection.updateOne({ _id: player._id }, { $set: { rank: player.rank } });
-//     });
-
-//     await Promise.all(updatePromises);
-
-//     // Respond to the client with a JSON string of players
-//     res.status(200).json(players);
-//   } catch (error) {
-//     // Handle error
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// }
+    // Respond to the client with a JSON string of players
+    res.status(200).json(players);
+  } catch (error) {
+    // Handle error
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
