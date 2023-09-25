cfd499c1c24cd506f33ff434e6b5149e3e19c70c
Engjell Ramadani <eramadani@wpi.edu>
2023-09-25 11:31:35 -0400

    Updated

--- server.improved.js
+++ server.improved.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+const User = require('./models/user')
+const Task = require('./models/task')
 
 // Configure MongoDB connection using the MONGO_STRING environment variable
 mongoose.connect(process.env.MONGO_STRING, {
@@ -21,23 +28,85 @@ mongoose.connect(process.env.MONGO_STRING, {
     console.error('Error connecting to MongoDB:', error);
   });
 
-
-// Define a schema for tasks using Mongoose
-const taskSchema = new mongoose.Schema({
-  task: String,
-  description: String, // Add task description field
-  dueDate: Date, // Add due date field
-  priority: String, // Add priority field
-  timestamp: Date,
-});
-
-// Create a model for tasks
-const Task = mongoose.model('Task', taskSchema);
-
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require("express-session")({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}));
+  
 app.use(bodyParser.json());
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// Showing home page
+app.get("/", function (req, res) {
+  res.render("login");
+});
+
+
+// Showing register form
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+// Handling user signup
+app.post("/register", async (req, res) => {
+  const user = await User.create({
+    username: req.body.username,
+    password: req.body.password
+  });
+  
+  return res.status(200).json(user);
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+//Handling user login
+app.post("/login", async function(req, res){
+  try {
+      // check if the user exists
+      const user = await User.findOne({ username: req.body.username });
+      if (user) {
+        //check if password matches
+        const result = req.body.password === user.password;
+        if (result) {
+          res.render("secret");
+        } else {
+          res.status(400).json({ error: "password doesn't match" });
+        }
+      } else {
+        res.status(400).json({ error: "User doesn't exist" });
+      }
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+});
+
+//Handling user logout 
+app.get("/logout", function (req, res) {
+  req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+});
+
+
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/index",
+  failureRedirect: "/login"
+}));
 
 // Handle POST requests to add tasks to the database
 app.post('/tasks', async (req, res) => {
