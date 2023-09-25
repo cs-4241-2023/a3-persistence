06f3565a06696593f22e0fc47a2071eaad51561d
Addy <boxturtle2003@gmail.com>
2023-09-24 22:19:03 -0400

    Update server.improved.js

--- server.improved.js
+++ server.improved.js
@@ -1,189 +1,240 @@
 const express = require("express");
+const cookieSession = require("cookie-session");
+const { MongoClient } = require('mongodb');
+const app = express();
 const fs = require("fs");
 const mime = require("mime");
-const MongoClient = require("mongodb").MongoClient;
-const bcrypt = require("bcrypt");
+const dir = "public/";
+const port = 3000;
+const uri = "mongodb+srv://boxturtle2003:AmCLH5ckA8WEICdT@cluster0.ejk65ad.mongodb.net/?retryWrites=true&w=majority";
 
-const app = express();
-const port = process.env.PORT || 3000;
-const mongoURL = "mongodb://localhost:27017";
-const dbName = "mydb";
-const collectionName = "recipes";
-const loginCollectionName = "users";
+const client = new MongoClient(uri);
+client.connect();
 
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
 
+app.get('/', (req, res) => {
+  sendFile(res, "public/index.html");
+});
 
-
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  const filename = "public/index.html";
-  sendFile(res, filename);
-});
+app.post('/login', (req, res) => {
+  let dataString = "";
 
-app.post("/add_recipe", (req, res) => {
-  const requestData = req.body;
-  addRecipeToMongo(requestData, res);
-});
+  req.on("data", function (data) {
+    dataString += data;
+  });
 
-app.post("/delete_recipe", (req, res) => {
-  const requestData = req.body;
-  deleteRecipeFromMongo(requestData.recipe_name, res);
-});
-
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  // Validate username and password with database
-});
-
-app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  MongoClient.connect(mongoURL, async function (err, client) {
-    if (err) {
-      console.error("MongoDB connection error:", err);
-      res.status(500).json({ message: "Internal server error" });
-      return;
+  req.on("end", function () {
+    const userData = JSON.parse(dataString);
+    const username = userData.username;
+    const password = userData.password;
+    const aUser = {
+      username: username,
+      password: password
     }
-
-    const db = client.db(dbName);
-    const userCollection = db.collection(loginCollectionName);
-
-    // Check if username already exists
-    const existingUser = await userCollection.findOne({ username });
-    if (existingUser) {
-      res.status(400).json({ message: "Username already exists" });
-      client.close();
-      return;
-    }
-
-    userCollection.insertOne(
-      { username, password: hashedPassword },
-      function (err, result) {
-        if (err) {
-          console.error("Error inserting user:", err);
-          res.status(500).json({ message: "Internal server error" });
-          client.close();
-          return;
-        }
-
-        console.log(`User added: ${result.insertedId}`);
-        res.status(200).json({ message: "User registered successfully" });
-        client.close();
-      }
-    );
+    loginHelper(aUser, res, req);
   });
 });
 
-const sendFile = function (res, filename) {
+async function loginHelper(aUser, res, req) {
+  try {
+
+    const DB = client.db("a3DB");
+    const collection = DB.collection("Users");
+
+    const name = aUser.username
+    const key = { username: name };
+    const exists = await collection.findOne(key);
+
+    if (exists === null) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ username: name }));
+    } else if (exists.password === aUser.password) {
+      console.log("Login Successful");
+      req.session.username = aUser.username;
+      res.redirect('/recipe.html');
+    } else {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ username: name }));
+    }
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
+app.post("/createUser", (request, response) => {
+
+  let dataString = "";
+
+  request.on("data", function (data) {
+    dataString += data;
+  });
+
+  request.on("end", function () {
+    const userData = JSON.parse(dataString);
+    request.session.username = userData.username;
+    if (userData.password == userData.REpassword) {
+      // If the username doesn't exist add a create a new user 
+      const newUser = {
+        username: request.session.username,
+        password: userData.password,
+        recipes: [],
+      };
+      addToDb(newUser, response);
+    } else {
+      console.log("Passwords do not match");
+      response.writeHead(400, { "Content-Type": "application/json" })
+      response.end(JSON.stringify({ username: request.session.username }));
+    }
+  });
+});
+
+app.delete("/remove", (request, response) => {
+
+  let dataString = "";
+
+  request.on("data", function (data) {
+    dataString += data;
+  });
+
+  request.on("end", function () {
+    const username = request.session.username;
+    const recipeName = JSON.parse(dataString).recipeName;
+    deleteRecipe(username, recipeName, response);
+  });
+});
+
+app.post("/addRecipe", (request, response) => {
+  let dataString = "";
+
+  request.on("data", function (data) {
+    dataString += data;
+  });
+
+  request.on("end", function () {
+    const username = request.session.username;
+    const recipeData = JSON.parse(dataString);
+    addRecipe(username, recipeData, response);
+  });
+});
+
+app.get("/getRecipes", (request, response) => {
+  const username = request.session.username;
+  getRecipes(username, response);
+});
+
+async function deleteRecipe(username, recipeName, res) {
+  try {
+    const DB = client.db("a3DB");
+    const collection = DB.collection("Users");
+
+    const key = { username: username };
+    const exists = await collection.findOne(key);
+
+    if (exists !== null) {
+      const updatedRecipes = exists.recipes.filter(recipe => recipe.name !== recipeName);
+      const update = { $set: { recipes: updatedRecipes } };
+      await collection.updateOne(key, update);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Recipe deleted successfully." }));
+    } else {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "User not found." }));
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function addRecipe(username, recipeData, res) {
+  try {
+    const DB = client.db("a3DB");
+    const collection = DB.collection("Users");
+
+    const key = { username: username };
+    const exists = await collection.findOne(key);
+
+    if (exists !== null) {
+      exists.recipes.push(recipeData);
+      const update = { $set: { recipes: exists.recipes } };
+      await collection.updateOne(key, update);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Recipe added successfully." }));
+    } else {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "User not found." }));
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getRecipes(username, res) {
+  try {
+    const DB = client.db("a3DB");
+    const collection = DB.collection("Users");
+
+    const key = { username: username };
+    const exists = await collection.findOne(key);
+
+    if (exists !== null) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(exists.recipes));
+    } else {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "User not found." }));
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function addToDb(newUser, res) {
+  try {
+    const DB = client.db("a3DB");
+    const collection = DB.collection("Users");
+
+    const name = newUser.username;
+    const key = { username: name };
+    const exists = await collection.findOne(key);
+
+    if (exists === null) {
+      await collection.insertOne(newUser);
+      res.type('html');
+      res.redirect('/recipe.html');
+
+    } else {
+      res.writeHead(300, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ username: newUser.username }));
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const sendFile = function (response, filename) {
   const type = mime.getType(filename);
+
   fs.readFile(filename, function (err, content) {
     if (err === null) {
-      res.writeHead(200, { "Content-Type": type });
-      res.end(content);
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
     } else {
-      res.writeHead(404);
-      res.end("404 Error: File Not Found");
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
     }
   });
 };
 
-// Define the addRecipeToMongo function
-const addRecipeToMongo = function (recipeData, res) {
-  MongoClient.connect(mongoURL, function (err, client) {
-    if (err) throw err;
-    
+app.use(express.static('public'));
 
-    const db = client.db(dbName);
-
-    const collection = db.collection(collectionName);
-
-    // Calculate cooking time based on ingredients complexity
-    const calculateCookingTime = (ingredients) => {
-      const ingredientCount = ingredients.split(",").length;
-      const cookingTime = ingredientCount * 5; // 5 minutes per ingredient
-      return cookingTime;
-    };
-
-    recipeData.cooking_time = calculateCookingTime(
-      recipeData.recipe_ingredients
-    );
-
-    // Insert the complete recipe data, including new fields
-    collection.insertOne(recipeData, function (err, result) {
-      if (err) throw err;
-
-      console.log(`Recipe added: ${result.insertedId}`);
-
-      res.status(200).json(result.ops);
-      client.close();
-    });
-  });
-};
-
-// Define the deleteRecipeFromMongo function
-const deleteRecipeFromMongo = function (recipeName, username, res) {
-  MongoClient.connect(mongoURL, function (err, client) {
-    if (err) {
-      console.error("MongoDB connection error:", err);
-      res.status(500).json({ message: "Internal server error" });
-      return;
-    }
-
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-
-    collection.deleteOne(
-      { recipe_name: recipeName, username: username },
-      function (err, result) {
-        if (err) {
-          console.error("Error deleting recipe:", err);
-          res.status(500).json({ message: "Internal server error" });
-          client.close();
-          return;
-        }
-
-        if (result.deletedCount === 0) {
-          res.status(404).json({ message: `Recipe ${recipeName} not found.` });
-        } else {
-          console.log(`Recipe deleted: ${recipeName}`);
-          res.status(200).json({ message: `Recipe ${recipeName} deleted.` });
-        }
-
-        client.close();
-      }
-    );
-  });
-};
-
-app.post("/delete_recipe", (req, res) => {
-  const recipeName = req.body.recipe_name;
-  const username = req.body.username; // Assuming you pass username in the request
-  deleteRecipeFromMongo(recipeName, username, res);
-});
-
-app.get("/get_recipes", async (req, res) => {
-  MongoClient.connect(mongoURL, async function (err, client) {
-    if (err) {
-      console.error("MongoDB connection error:", err);
-      res.status(500).json({ message: "Internal server error" });
-      return;
-    }
-
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-
-    // Fetch all recipes from the database
-    const recipes = await collection.find().toArray();
-
-    res.status(200).json(recipes);
-    client.close();
-  });
-});
-
-// Start the Express server
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Server is running on port ${port}`);
 });
