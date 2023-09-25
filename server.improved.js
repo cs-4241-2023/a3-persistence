89f3ea6c0e0cb4d2dcf95947133072199ce24396
taccat3 <tcaplan172@gmail.com>
2023-09-25 16:37:38 -0400

    getting github oauth set up

--- server.improved.js
+++ server.improved.js
@@ -194,7 +194,7 @@ app.get("/github-callback", (request, response) => {
     .then((res) => res.data.access_token)
     .then((token) => {
       accessToken = token;
-      res.redirect(`/?token=${token}`);
+      response.redirect(`/?token=${token}`);
     })
     .catch((err) => response.status(500).json({ err: err.message }));
 });
