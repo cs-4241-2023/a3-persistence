83d0465d4c4f15be7a1b1770bf144910c9fb8598
MarekSGarbaczonek <66498850+MarekSGarbaczonek@users.noreply.github.com>
2023-09-25 11:39:04 -0400

    Changes

--- server.js
+++ server.js
@@ -7,25 +7,6 @@ require('dotenv').config();
 const mongoDBPASSW = process.env.MONGOPASSW;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://msgarbaczonek:"+mongoDBPASSW+"@a3.zqvh72t.mongodb.net/?retryWrites=true&w=majority";
-const passport = require('passport');
-const GitHubStrategy = require('passport-github2').Strategy;
-const mongoose = require('mongoose');
-const session = require('express-session');
-
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-
-app.use(session({
-  secret: process.env.SESSECRET,
-  resave: false,
-  saveUninitialized: true,
-}));
-
-app.use(passport.initialize());
 
 let appdata = [
   { TaskName: "Task 1", DueDate: "09/12/2023", Priority: 1, MyDay: true },
@@ -102,10 +83,8 @@ app.get('/appdata', async (req, res) => {
   res.json(documents);
 });
 
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  res.render('login', { user: req.user });
 });
 
 function errorHandler(err, req, res, next) {
@@ -114,52 +93,6 @@ function errorHandler(err, req, res, next) {
 }
 
 app.use(errorHandler);
-
-const userSchema = new mongoose.Schema({
-  githubId: String,
-  githubUsername: String
-});
-const User = mongoose.model('User', userSchema);
-module.exports = User;
-
-passport.use(new GitHubStrategy({
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: process.env.CALLBACK
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    let user = await User.findOne({ githubId: profile.id });
-
-    if (!user) {
-      user = new User({
-        githubId: profile.id,
-        githubUsername: profile.username,
-      });
-
-      await user.save();
-    }
-    return done(null, user);
-  } catch (err) {
-    return done(err);
-  }
-}));
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
-});
-
-app.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email' ] }));
-app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }),
-function(req, res) {
-  console.log('Authenticated user:', req.user);
-  res.redirect('/');
-});
 
 const listener = app.listen( process.env.PORT || 3000 )
 
@@ -173,29 +106,6 @@ const listener = app.listen( process.env.PORT || 3000 )
 
 
 
-
-
-
-
-
-
-
-
-
-
-// User.deleteMany({}, (err) => {
-//   if (err) {
-//     console.error('Error deleting users:', err);
-//   } else {
-//     console.log('All users have been removed.');
-//   }
-
-//   // Close the database connection
-//   mongoose.connection.close();
-// });show
-
-
-
 // const express = require('express');
 // const app = express();
 // const path = require('path');
@@ -205,6 +115,28 @@ const listener = app.listen( process.env.PORT || 3000 )
 // const mongoDBPASSW = process.env.MONGOPASSW;
 // const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const uri = "mongodb+srv://msgarbaczonek:"+mongoDBPASSW+"@a3.zqvh72t.mongodb.net/?retryWrites=true&w=majority";
+// const passport = require('passport');
+// const GitHubStrategy = require('passport-github2').Strategy;
+// const mongoose = require('mongoose');
+// const session = require('express-session');
+
+
+// mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// const db = mongoose.connection;
+
+// app.use(session({
+//   secret: process.env.SESSECRET,
+//   resave: false,
+//   saveUninitialized: true,
+// }));
+
+// app.use(passport.initialize());
+
+
 
 // let appdata = [
 //   { TaskName: "Task 1", DueDate: "09/12/2023", Priority: 1, MyDay: true },
@@ -281,8 +213,10 @@ const listener = app.listen( process.env.PORT || 3000 )
 //   res.json(documents);
 // });
 
+
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//   res.render('login', { user: req.user });
 // });
 
 // function errorHandler(err, req, res, next) {
@@ -291,5 +225,90 @@ const listener = app.listen( process.env.PORT || 3000 )
 // }
 
 // app.use(errorHandler);
+
+
+
+
+// // passport.use(new GitHubStrategy({
+// //   clientID: process.env.CLIENT_ID,
+// //   clientSecret: process.env.CLIENT_SECRET,
+// //   callbackURL: process.env.CALLBACK
+// // },
+// // function(accessToken, refreshToken, profile, done) {
+// //   User.findOrCreate({ githubId: profile.id }, function (err, user) {
+// //     return done(err, user);
+// //   });
+// // }
+// // ));
+
+// // app.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email' ] }));
+// // app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }),
+// // function(req, res) {
+// //   res.redirect('/');
+// // });
+
+
+
+// const userSchema = new mongoose.Schema({
+//   githubId: String,
+//   githubUsername: String
+// });
+// const User = mongoose.model('User', userSchema);
+// module.exports = User;
+
+// passport.use(new GitHubStrategy({
+//   clientID: process.env.CLIENT_ID,
+//   clientSecret: process.env.CLIENT_SECRET,
+//   callbackURL: process.env.CALLBACK
+// }, async (accessToken, refreshToken, profile, done) => {
+//   try {
+//     // console.log(profile.id);
+//     // console.log(profile.username);
+//     // const allUsers = await User.find();
+//     // console.log('All Users:', allUsers);
+//     let user = await User.findOne({ githubId: profile.id });
+
+//     if (!user) {
+//       user = new User({
+//         githubId: profile.id,
+//         githubUsername: profile.username,
+//       });
+
+//       await user.save();
+//     }
+//     return done(null, user);
+//   } catch (err) {
+//     return done(err);
+//   }
+// }));
+
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// });
+
+// passport.deserializeUser((id, done) => {
+//   User.findById(id, (err, user) => {
+//     done(err, user);
+//   });
+// });
+
+// app.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email' ] }));
+// app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }),
+// function(req, res) {
+//   console.log('Authenticated user:', req.user);
+//   res.redirect('/');
+// });
+
+
+// // User.deleteMany({}, (err) => {
+// //   if (err) {
+// //     console.error('Error deleting users:', err);
+// //   } else {
+// //     console.log('All users have been removed.');
+// //   }
+
+// //   // Close the database connection
+// //   mongoose.connection.close();
+// // });show
 
 // const listener = app.listen( process.env.PORT || 3000 )
