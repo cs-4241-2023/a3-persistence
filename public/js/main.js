8f353b85d9049fc70dd8dea1dece2606a538b88e
Addy <boxturtle2003@gmail.com>
2023-09-25 01:13:44 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,185 +1,296 @@
-// FRONT-END (CLIENT) JAVASCRIPT HERE
+// Fetch recipes and display them
+const getRecipes = async function () {
+  try {
+    // Fetch and display the user's recipe list
+    const response = await fetch("/getRecipes");
+    const contentType = response.headers.get("content-type");
+    console.log("About to enter if in main.js.");
 
+    if (contentType && contentType.includes("application/json")) {
+      const data = await response.json();
+      if (response.ok) {
+        // Success
+        displayRecipes(data);
+      } else {
+        console.error("Error: " + response.status);
+        displayError("Error fetching recipes.");
+      }
+    } else {
+      console.error("Unexpected content type:", contentType);
+      console.log("Response as text:", await response.text());
+      // Handle other unexpected content types here.
+    }
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+    displayError("Error fetching recipes.");
+  }
+};
+// Define the addRecipe function
 const addRecipe = async function (event) {
   event.preventDefault();
 
-  const inputName = document.querySelector("#recipe_name");
-  const inputIngredients = document.querySelector("#recipe_ingredients");
-  const inputDirections = document.querySelector("#recipe_directions");
-
   const recipeData = {
-    recipe_name: inputName.value,
-    recipe_ingredients: inputIngredients.value,
-    recipe_directions: inputDirections.value,
+    recipe_name: document.getElementById("recipe_name").value,
+    recipe_ingredients: document.getElementById("recipe_ingredients").value,
+    recipe_directions: document.getElementById("recipe_directions").value,
   };
 
-  const response = await fetch("/add_recipe", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(recipeData),
-  });
-  console.log(response);
-  const recipes = await response.json();
+  // Calculate the cooking time based on the number of steps and ingredients
+  const numSteps = recipeData.recipe_directions.split(/\r\n|\r|\n/).length;
+  const numIngredients =
+    recipeData.recipe_ingredients.split(/\r\n|\r|\n/).length;
 
-  const tableRows = recipes
-    .map((recipe) => {
-      return `<tr>
-        <td>${recipe.recipe_name}</td>
-        <td>${recipe.recipe_ingredients}</td>
-        <td>${recipe.recipe_directions}</td>
-        <td>${recipe.cooking_time} minutes</td> <!-- Display Cooking Time -->
-     </tr>`;
-    })
-    .join("");
+  const cookingTime = numSteps + numIngredients;
 
-  document.querySelector("#recipe_table").innerHTML = tableRows;
+  recipeData.cooking_time = cookingTime;
 
-  inputName.value = "";
-  inputIngredients.value = "";
-  inputDirections.value = "";
-};
-
-const deleteRecipe = async function (event) {
-  event.preventDefault();
-
-  const inputToDelete = document.querySelector("#recipe_to_delete");
-
-  const recipeToDelete = {
-    recipe_name: inputToDelete.value,
-  };
-
-  const response = await fetch("/delete_recipe", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(recipeToDelete),
-  });
-
-  const recipes = await response.json();
-
-  const tableRows = recipes
-    .map((recipe) => {
-      return `<tr>
-        <td>${recipe.recipe_name}</td>
-        <td>${recipe.recipe_ingredients}</td>
-        <td>${recipe.recipe_directions}</td>
-        <td>${recipe.cooking_time} minutes</td> <!-- Display Cooking Time -->
-      </tr>`;
-    })
-    .join("");
-
-  document.querySelector("#recipe_table").innerHTML = tableRows;
-
-  inputToDelete.value = "";
-};
-
-window.onload = function () {
-  const addRecipeButton = document.getElementById("add_recipe");
-  const deleteRecipeButton = document.getElementById("delete_recipe");
-
-  addRecipeButton.onclick = addRecipe;
-  deleteRecipeButton.onclick = deleteRecipe;
-  const login = async function (event) {
-    event.preventDefault();
-
-    const username = document.querySelector("#username_login").value;
-    const password = document.querySelector("#password_login").value;
-
-    const userData = {
-      username,
-      password,
-    };
-
-    const response = await fetch("/login", {
+  try {
+    const response = await fetch("/addRecipe", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(userData),
-    });
-  };
-
-  const register = async function (event) {
-    event.preventDefault();
-
-    const username = document.querySelector("#username_register").value;
-    const password = document.querySelector("#password_register").value;
-
-    const userData = {
-      username,
-      password,
-    };
-
-    try {
-      const response = await fetch("/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-      });
-
-      const data = await response.json();
-
-      if (response.ok) {
-        console.log(data.message);
-        // Handle successful registration here, perhaps navigate to a new page or update UI
-        alert("Registration successful!");
-      } else {
-        console.error(data.message);
-        // Handle errors here, perhaps update the UI to show an error message
-        alert(`Registration failed: ${data.message}`);
-      }
-    } catch (error) {
-      console.error("An error occurred:", error);
-      // Handle network or other errors here
-      alert("An error occurred while registering. Please try again.");
-    }
-  };
-
-  const displayRecipes = async () => {
-    const response = await fetch("/get_recipes", {
-      method: "GET",
+      body: JSON.stringify(recipeData),
     });
 
     if (response.ok) {
-      const recipes = await response.json();
-
-      const tableRows = recipes
-        .map((recipe) => {
-          return `<tr>
-          <td>${recipe.recipe_name}</td>
-          <td>${recipe.recipe_ingredients}</td>
-          <td>${recipe.recipe_directions}</td>
-          <td>${recipe.cooking_time} minutes</td>
-        </tr>`;
-        })
-        .join("");
-
-      document.querySelector("#recipe_table").innerHTML = tableRows;
+      // Recipe added successfully
+      console.log("Recipe added successfully");
     } else {
-      console.error("Failed to fetch recipes.");
+      // Error adding the recipe
+      console.error("Error adding recipe");
     }
+  } catch (error) {
+    console.error("Error adding recipe:", error);
+  }
+};
+document.addEventListener("DOMContentLoaded", function () {
+  getRecipes();
+  displayRecipes();
+  // Login
+  const loginButton = document.querySelector("#login_button");
+  loginButton.addEventListener("click", login);
+
+  // Register
+  const registerButton = document.querySelector("#register_button");
+  registerButton.addEventListener("click", createUser);
+
+  // Add Recipe
+  const addRecipeButton = document.querySelector("#add_recipe");
+  addRecipeButton.addEventListener("click", addRecipe);
+
+  // Delete Recipe
+  const deleteRecipeButton = document.querySelector("#delete_recipe");
+  deleteRecipeButton.addEventListener("click", deleteRecipe);
+
+  // Initialize the recipe list
+  getRecipes();
+});
+
+const login = async function (event) {
+  event.preventDefault();
+
+  // Get inputs
+  const usernameInput = document.querySelector("#username_login");
+  const passwordInput = document.querySelector("#password_login");
+
+  // Parse as strings
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  // Create a new array with data
+  const userData = {
+    username: username,
+    password: password,
   };
 
-  // Add an event listener to call the displayRecipes function when the page loads
-  window.onload = function () {
-    // Event handlers for adding and deleting recipes
-    const addRecipeButton = document.getElementById("add_recipe");
-    const deleteRecipeButton = document.getElementById("delete_recipe");
-    addRecipeButton.onclick = addRecipe;
-    deleteRecipeButton.onclick = deleteRecipe;
+  // Pass newUser to the server and wait for a response
+  const response = await fetch("/login", {
+    method: "POST",
+    body: JSON.stringify(userData),
+  });
 
-    // Event handlers for login and registration
-    const loginButton = document.getElementById("login_button");
-    const registerButton = document.getElementById("register_button");
-    loginButton.onclick = login;
-    registerButton.onclick = register;
+  const nameDisplay = document.querySelector("#error-message");
 
-    // Fetch and display recipes when the page loads
-    displayRecipes();
+  const contentType = response.headers.get("content-type");
+
+  if (contentType && contentType.includes("application/json")) {
+    try {
+      const data = await response.json();
+      if (response.status === 404) {
+        nameDisplay.textContent = `Username: ${data.username} could not be found.`;
+      } else if (response.status === 400) {
+        console.log("Incorrect password");
+        nameDisplay.textContent = `Password for ${data.username} was incorrect `;
+      } else {
+        console.error("Error: " + response.status);
+        nameDisplay.textContent = "Error: " + response.status;
+      }
+    } catch (error) {
+      console.error("Error while parsing JSON:", error);
+      console.log("Response as HTML:", await response.text());
+      // You might display the HTML response or handle it differently.
+    }
+  } else if (contentType && contentType.includes("text/html")) {
+    // Handle the HTML response
+    const htmlResponse = await response.text();
+    // Display the HTML response or handle it as needed
+    // For example, you can set the content of an element with the HTML
+    document.documentElement.innerHTML = htmlResponse;
+    window.location.replace("/index.html");
+  } else {
+    console.error("Unexpected content type:", contentType);
+    console.log("Response as text:", await response.text());
+    // Handle other unexpected content types here.
+  }
+};
+
+const createUser = async function (event) {
+  event.preventDefault();
+
+  // Get inputs
+  const usernameInput = document.querySelector("#username_register");
+  const passwordInput = document.querySelector("#password_register");
+  const REpasswordInput = document.querySelector("#REpassword");
+  const balanceInput = document.querySelector("#balance");
+
+  // Parse as strings and floats
+  const balance = parseFloat(balanceInput.value);
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  const REpassword = REpasswordInput.value;
+
+  // Create a new array with data
+  const userData = {
+    username: username,
+    password: password,
+    REpassword: REpassword,
+    balance: balance,
   };
+
+  // Pass newUser to the server and wait for a response
+  const response = await fetch("/createUser", {
+    method: "POST",
+    body: JSON.stringify(userData),
+  });
+
+  const nameDisplay = document.querySelector("#nameDisplay");
+
+  if (response.status === 302) {
+    // Handle the redirection here, if needed
+    console.log("Redirecting...");
+    // You might also directly redirect the client, like this:
+    // window.location.href = response.url;
+  } else {
+    const contentType = response.headers.get("content-type");
+
+    if (contentType && contentType.includes("application/json")) {
+      try {
+        const data = await response.json();
+        if (response.status === 200) {
+          console.log("Success: User created");
+          nameDisplay.textContent = `New User Created: ${data.username}!`;
+        } else if (response.status === 300) {
+          nameDisplay.textContent = `Username: ${data.username} Already Exists.`;
+        } else if (response.status === 301) {
+          console.log("Passwords do not match");
+          nameDisplay.textContent = "Passwords do not match";
+        } else {
+          console.error("Error: " + response.status);
+          nameDisplay.textContent = "Error: " + response.status;
+        }
+      } catch (error) {
+        console.error("Error while parsing JSON:", error);
+        console.log("Response as HTML:", await response.text());
+        // You might display the HTML response or handle it differently.
+      }
+    } else if (contentType && contentType.includes("text/html")) {
+      // Handle the HTML response
+      const htmlResponse = await response.text();
+      // Display the HTML response or handle it as needed
+      // For example, you can set the content of an element with the HTML
+      document.documentElement.innerHTML = htmlResponse;
+      window.location.replace("/index.html");
+    } else {
+      console.error("Unexpected content type:", contentType);
+      console.log("Response as text:", await response.text());
+      // Handle other unexpected content types here.
+    }
+  }
+};
+
+// Add Recipe Form Submission
+const addRecipeButton = document.querySelector("#add_recipe");
+addRecipeButton.addEventListener("click", addRecipe);
+
+const deleteRecipe = async function (event) {
+  event.preventDefault();
+
+  // Get inputs
+  const recipeNameInput = document.querySelector("#recipe_to_delete");
+  const usernameInput = document.querySelector("#username_to_delete");
+
+  // Parse as strings
+  const recipeName = recipeNameInput.value;
+  const username = usernameInput.value;
+
+  // Create a new array with data
+  const deleteData = {
+    recipeName: recipeName,
+    username: username,
+  };
+
+  // Pass the new array to the server and wait for a response
+  const response = await fetch("/remove", {
+    method: "DELETE",
+    body: JSON.stringify(deleteData),
+  });
+
+  const deleteStatus = document.querySelector("#delete_status");
+
+  if (response.status === 200) {
+    // Success
+    deleteStatus.textContent = `Recipe '${recipeName}' deleted successfully.`;
+    // Clear the input fields
+    recipeNameInput.value = "";
+    usernameInput.value = "";
+    // Refresh the recipe list
+    getRecipes();
+  } else if (response.status === 404) {
+    deleteStatus.textContent = `Recipe '${recipeName}' not found for user '${username}'.`;
+  } else {
+    console.error("Error: " + response.status);
+    deleteStatus.textContent = "Error deleting the recipe.";
+  }
+};
+
+// Display recipes with error handling
+const displayRecipes = function (recipes) {
+  const recipeTable = document.querySelector("#recipe_table");
+  recipeTable.innerHTML = ""; // Clear previous data
+
+  if (!recipes || recipes.length === 0) {
+    recipeTable.innerHTML = "<tr><td colspan='4'>No recipes found.</td></tr>";
+  } else {
+    recipes.forEach((recipe) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+        <td>${recipe.recipe_name}</td>
+        <td>${recipe.recipe_ingredients}</td>
+        <td>${recipe.recipe_directions}</td>
+        <td>${recipe.cooking_time}</td>
+      `;
+      recipeTable.appendChild(row);
+    });
+  }
+};
+
+const displayNoRecipes = function () {
+  const recipeTable = document.querySelector("#recipe_table");
+  recipeTable.innerHTML = "<tr><td colspan='4'>No recipes found.</td></tr>";
+};
+
+const displayError = function (message) {
+  const recipeTable = document.querySelector("#recipe_table");
+  recipeTable.innerHTML = `<tr><td colspan='4'>${message}</td></tr>`;
 };
