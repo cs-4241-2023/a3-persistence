e38114b66acc4052f0786c8295241b412806d194
Bright Lin <brightlin183@gmail.com>
2023-09-24 21:43:58 -0400

    heroku update

--- server.improved.js
+++ server.improved.js
@@ -26,7 +26,7 @@ run();
 const express = require('express'),
       cookie = require ('cookie-session'),
       app = express(),
-      port = 3000
+      port = process.env.PORT || 3000
 
 
 app.use(express.json())
