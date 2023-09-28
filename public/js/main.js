b8119c79b4a1ee7af91cb2931f7b008152f50323
AarshZadaphiya <aszadaphiya@wpi.edu>
2023-09-27 21:01:37 -0400

    login progress

--- public/js/main.js
+++ public/js/main.js
@@ -3,65 +3,7 @@ const submit = async function (event) {
   // a new .html page for displaying results...
   // this was the original browser behavior and still
   // remains to this day
-  event.preventDefault();
-  let evt = event.target
-  let json = {}
-  if (evt.getAttribute('formaction') === '/submit'){
-    json = {
-      tasks: document.querySelector("#taskName").value,
-      date: document.querySelector("#dueDate").value,
-      priority: document.querySelector("#taskPriority").value,
-    }
-   }else {
-    json={id:evt.getAttribute('id')}
-   }
-
-    fetch(evt.getAttribute('formaction'), {
-      method: "POST",
-      body: JSON.stringify(json),
-    }).then(async function (response) {
-      let data = await response.json();
-
-    const tableparse = document.querySelector("tbody");
-    tableparse.innerHTML = "";
-
-    let resultHTML = tableparse.innerHTML;
-    for (const key in data) {
-      if (data.hasOwnProperty(key)) {
-        const row = document.createElement("tr");
-       row.id = data[key].id;
-
-        // for loop for each cell
-        for (const cellKey in data[key]) {
-          const cell = document.createElement("td");
-          if (cellKey === "daysRemaining") {
-            const daysRemainingCell = document.createElement("td");
-            daysRemainingCell.innerText = data[key][cellKey];
-            row.appendChild(daysRemainingCell);
-          } else {
-            cell.innerText = data[key][cellKey];
-            row.appendChild(cell);
-          }
-          
-        }
-
-        const deleteButton = document.createElement("button");
-        deleteButton.innerText = "Delete";
-        deleteButton.className = "my-deletebutton";
-        deleteButton.formAction = '/delete'
-        deleteButton.id=data[key].id;
-
-        deleteButton.onclick = submit
-
-        const deleteCell = document.createElement("td");
-        deleteCell.appendChild(deleteButton);
-        row.appendChild(deleteCell);
-
-        tableparse.appendChild(row);
-
-      }
-    }
-  });
+  
 };
 
 window.onload = function () {
--- server.improved.js
+++ server.improved.js
@@ -31,17 +31,17 @@ async function run() {
 app.post("/login", async (req, res) => {
   const username= req.body.username;
   const password = req.body.password;
-  const user = await collection.findOne({username : username },{ password: password });
+  const user = await collection.findOne({username : username, password: password });
 
-  if (user) {
+  console.log("username: " + username);
+  if (user !== null) {
    //res.json({ success: true, message: "Login successful" });
    console.log("Login successful")
-    res.redirect('/public/bmi.html');
+    res.redirect('bmi.html');
   } else {
     res.status(401).json({ success: false, message: "Login failed" });
   }
 });
-
 
 app.listen(3000)
 
