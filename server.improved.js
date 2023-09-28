02f857a2496f9fcee4d4f7bb934d1dc9349bde6f
AarshZadaphiya <aszadaphiya@wpi.edu>
2023-09-27 23:48:18 -0400

    Login redirect update

--- server.improved.js
+++ server.improved.js
@@ -3,46 +3,48 @@ const express = require("express"),
       app = express()
 
 app.use(express.static("public") )
-app.use(express.json() )
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
-require("dotenv").config()
-
-//port: process.env.PORT || 3000
-
-//const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`
 const uri = 'mongodb+srv://aszadaphiya:aarsh@a3-aarshzadaphiya.rnuxw22.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient( uri )
-
+const bcrypt = require('bcrypt');  
 let collection = null
 
-async function run() {
+const dbConnect = async function() {
   await client.connect()
   collection = await client.db("a3").collection("Users")
-
-  // route to get all docs
+ 
   app.get("/docs", async (req, res) => {
     if (collection !== null) {
       const docs = await collection.find({}).toArray()
       res.json( docs )
-    }
+    } 
   })
+ 
+  app.post("/login", async (req, res) => {
+    try{
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+      const username= req.body.username;
+    const password = req.body.password;
+    console.log(req.body)  
+    const query = {$and: [{username: username, password: password}]}
+    const user = await collection.findOne(query);
+  
+    if (user !== null) {    
+      res.sendFile(__dirname + '/public/bmi.html');
+    } else {
+      res.status(401).json({ success: false, message: "Login failed" });
+    }
+    } catch {
+      res.status(500).send()  
+    }
+    
+  });
+  
 }
 
-app.post("/login", async (req, res) => {
-  const username= req.body.username;
-  const password = req.body.password;
-  const user = await collection.findOne({username : username, password: password });
-
-  console.log("username: " + username);
-  if (user !== null) {
-   //res.json({ success: true, message: "Login successful" });
-   console.log("Login successful")
-    res.redirect('bmi.html');
-  } else {
-    res.status(401).json({ success: false, message: "Login failed" });
-  }
-});
+dbConnect()
 
 app.listen(3000)
-
-run()
