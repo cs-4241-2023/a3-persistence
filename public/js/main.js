791741cacfff3cf511172a3b8e611e220afd6b70
AarshZadaphiya <aszadaphiya@wpi.edu>
2023-10-01 15:40:10 -0400

    Updated UI for Lighthouse

--- public/js/main.js
+++ public/js/main.js
@@ -1,139 +1,137 @@
-let editIndex = null
-let userData = null
+let editIndex = null;
+let userData = null;
 
 const submit = async function (event) {
-
   event.preventDefault();
-  let evt = event.target
-  let json = {}
-  if (evt.getAttribute('formaction') === '/submit'){
+  let evt = event.target;
+  let json = {};
+  if (evt.getAttribute("formaction") === "/submit") {
     json = {
       height: document.querySelector("#height").value,
       weight: document.querySelector("#weight").value,
       age: document.querySelector("#age").value,
       gender: document.querySelector('input[name="gender"]:checked').value,
-    }
-    let btnSubmit = document.getElementById('Calculate')
-    btnSubmit.formAction = '/submit'
-   }else if (evt.getAttribute('formaction') === '/delete'){
-    json={id:evt.getAttribute('id')}
-   } else if (evt.getAttribute('formaction') === '/edit'){
-      //add the index to the json
-      //change button form action to submit
-      //reset the edit index
-      json = {
-        id: editIndex,
-        height: document.querySelector("#height").value,
-        weight: document.querySelector("#weight").value,
-        age: document.querySelector("#age").value,
-        gender: document.querySelector('input[name="gender"]:checked').value,
-      }
-      serverData[0]['data'][editIndex] = json
-   }
+    };
+    let btnSubmit = document.getElementById("Calculate");
+    btnSubmit.formAction = "/submit";
+  } else if (evt.getAttribute("formaction") === "/delete") {
+    json = { id: evt.getAttribute("id") };
+  } else if (evt.getAttribute("formaction") === "/edit") {
+    // add the index to the json
+    // change button form action to submit
+    // reset the edit index
+    json = {
+      id: editIndex,
+      height: document.querySelector("#height").value,
+      weight: document.querySelector("#weight").value,
+      age: document.querySelector("#age").value,
+      gender: document.querySelector('input[name="gender"]:checked').value,
+    };
+    serverData[0]["data"][editIndex] = json;
+  }
 
-    await fetch(evt.getAttribute('formaction'), {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(json),
-    }).then(async function(response) {
-      let data = await response.json();
-      populateTable(data)
-      let btnSubmit = document.getElementById('Calculate')
-      btnSubmit.formAction = '/submit'
-      document.getElementById('bmiForm').reset()
+  // Calculate BMI and update the span element
+  const calculatedBMI = calculateBMI(json.height, json.weight, json.age, json.gender);
+  document.getElementById("bmi").textContent = `BMI: ${calculatedBMI}`;
+
+  await fetch(evt.getAttribute("formaction"), {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(json),
+  }).then(async function (response) {
+    let data = await response.json();
+    populateTable(data);
+    let btnSubmit = document.getElementById("Calculate");
+    btnSubmit.formAction = "/submit";
+    document.getElementById("bmiForm").reset();
   });
 };
 
-
 function calculateBMI(height, weight, age, gender) {
   let bmi;
-  if (gender === 'male') {
-      bmi = (0.74 * weight) / (height/100 * height/100);
-      bmi += (0.063 * age) - 3.3;
-  } else if (gender === 'female') {
-      bmi = (0.74 * weight) / (height * height);
-      bmi += (0.063 * age) + 5.1;
+  if (gender === "male") {
+    bmi = (0.74 * weight) / (((height / 100) * height) / 100);
+    bmi += 0.063 * age - 3.3;
+  } else if (gender === "female") {
+    bmi = (0.74 * weight) / (height * height);
+    bmi += 0.063 * age + 5.1;
   } else {
-      throw new Error('Invalid gender');
+    throw new Error("Invalid gender");
   }
 
   return parseFloat(bmi.toFixed(2));
 }
 
-function editRow(event){
+function editRow(event) {
   //get the row from the table using the index and populate the form
   //change the button form action to edit
   //set the edit index
-  let evt = event.target
-  let rowId = evt.getAttribute('id')
-  editIndex = rowId
-  document.getElementById('height').value = serverData[0]['data'][rowId].height
-  document.getElementById('weight').value = serverData[0]['data'][rowId].weight
-  document.getElementById('age').value = serverData[0]['data'][rowId].age
-  if (serverData[0]['data'][rowId].gender === 'male') 
-     document.getElementById('male').checked = true
-  else
-     document.getElementById('female').checked = true;
-  let btnSubmit = document.getElementById('Calculate')
-  btnSubmit.formAction = '/edit'
+  let evt = event.target;
+  let rowId = evt.getAttribute("id");
+  editIndex = rowId;
+  document.getElementById("height").value = serverData[0]["data"][rowId].height;
+  document.getElementById("weight").value = serverData[0]["data"][rowId].weight;
+  document.getElementById("age").value = serverData[0]["data"][rowId].age;
+  if (serverData[0]["data"][rowId].gender === "male")
+    document.getElementById("male").checked = true;
+  else document.getElementById("female").checked = true;
+  let btnSubmit = document.getElementById("Calculate");
+  btnSubmit.formAction = "/edit";
 }
 
-function populateTable(data){
-  serverData = data
+function populateTable(data) {
+  serverData = data;
 
   const tableparse = document.querySelector("tbody");
-    tableparse.innerHTML = "";
+  tableparse.innerHTML = "";
 
-    let resultHTML = tableparse.innerHTML;
+  let resultHTML = tableparse.innerHTML;
 
-    let userData = data[0]['data']
+  let userData = data[0]["data"];
 
-    userData.forEach(function (data, index){
-      row = document.createElement("tr");
-      row.id = index;
-      
-      for (const cellKey in data) {
-        const cell = document.createElement("td");
-          cell.innerText = data[cellKey];
-          row.appendChild(cell);
-  
-      }
-      const deleteButton = document.createElement("button");
-      deleteButton.innerText = "Delete";
-      deleteButton.className = "my-deletebutton";
-      deleteButton.formAction = '/delete'
-      deleteButton.id=index;
-      deleteButton.onclick = submit
+  userData.forEach(function (data, index) {
+    row = document.createElement("tr");
+    row.id = index;
 
-      const deleteCell = document.createElement("td");
-      deleteCell.appendChild(deleteButton);
-      const editButton = document.createElement("button");
-      editButton.innerText = "Edit";
-      editButton.className = "my-editbutton";
-      editButton.formAction = '/edit'
-      editButton.id=index;
-      editButton.onclick = editRow
-      deleteCell.appendChild(editButton);
-      row.appendChild(deleteCell);
+    for (const cellKey in data) {
+      const cell = document.createElement("td");
+      cell.innerText = data[cellKey];
+      row.appendChild(cell);
+    }
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "btn btn-danger";
+    deleteButton.formAction = "/delete";
+    deleteButton.id = index;
+    deleteButton.onclick = submit;
 
-      tableparse.appendChild(row)
+    const deleteCell = document.createElement("td");
+    deleteCell.appendChild(deleteButton);
+    const editButton = document.createElement("button");
+    editButton.innerText = "Edit";
+    editButton.className = "btn btn-primary";
+    editButton.formAction = "/edit";
+    editButton.id = index;
+    editButton.onclick = editRow;
+    deleteCell.appendChild(editButton);
+    row.appendChild(deleteCell);
 
-    });
-      }
-
-
-async function fetchData(){
-  await fetch('/docs', {
-    method: 'GET',
-  }).then(async function(response) {
-    let data = await response.json()
-    populateTable(data)
-  })
+    tableparse.appendChild(row);
+  });
 }
 
-fetchData()
+async function fetchData() {
+  await fetch("/docs", {
+    method: "GET",
+  }).then(async function (response) {
+    let data = await response.json();
+    populateTable(data);
+  });
+}
+
+fetchData();
 
 window.onload = function () {
   const button = document.querySelector("button");
