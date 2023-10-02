bab8077e32b1681dd51f3756dadc60468d14fb38
scriblesean <sean.arackal@gmail.com>
2023-10-02 15:26:00 -0400

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require("express"),
 
 require("dotenv").config();
 
-app.use(express.static("./views/"));
+app.use(express.static("./views/"));    
 app.use( express.json() )
 app.use( express.urlencoded({ extended:true }) )
 app.use(helmet());
